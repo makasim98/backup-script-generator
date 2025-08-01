b70b78feb8899c8f30339b0357badbb5bf01ba82
makasim98 <maxluch98@gmail.com>
2025-08-01 14:47:25 -0400

    First Commit - Script Skeleton
    Co-authored-by: Nicolas Buu <NickBzy@users.noreply.github.com>

---
+++ generator.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo "====================================================================================="
+echo "Welcome to your interactive backup system"
+echo "====================================================================================="
+
+list_curr_config() {}
+add_bckp_src() {}
+add_bckp_target() {}
+get_bckp_freq() {}
+get_max_history() {}
+get_bckp_format() {}
+generate_script() {}
+
+# Interactive menu
+# l) List current configurations (Summary of configured options)
+# a) Add backup source (the directory/file we want to backup)
+# t) Add backup target (Path to store the backup at)
+# c) Frequency (cron for sheduing)
+# h) History (How much/old do we keep before descarding the oldest one)
+#       - Max number (discrad oldes after # buckups)
+#       - Max age (discard backups older then AGE)
+# f) Format (How to store it - tar, gzip, zip)
+# g) Generate the Script from current config
+# q) Quit (Cancel generation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
